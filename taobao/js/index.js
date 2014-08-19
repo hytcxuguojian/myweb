fed0b505368645832719087bd439adc1eaaf225a
zixuwuyou <1075737942@qq.com>
2014-08-19 23:41:17 +0800

    完善淘宝网已买到的宝贝页
    完成了布局和大部分JS。

--- taobao/js/index.js
+++ taobao/js/index.js
@@ -1,4 +1,30 @@
 $(function(){
+	/*Nav标题栏移动事件*/
+	$(".list li").hover(function(){
+		var left=$(this).attr("left");
+		$(".line").animate({"left":left},200);
+		return false;
+	},function(){});
+	$(".list").hover(function(){},function(){
+		$(".line").animate({"left":"0"});
+	});
+
+	/*左侧导航栏下拉菜单*/
+	$(".mt-indicator").click(function(){
+		var openstate=$(this).attr("isopen");
+		if (openstate=="true") 
+		{
+			$(this).attr("isopen","false");
+			$(this).css("background-position","0 -89px");
+			$(this).parent().find(".mt-menu-sub-content").hide();
+		}
+		if (openstate=="false") 
+		{
+			$(this).attr("isopen","true");
+			$(this).css("background-position","0 -101px");
+			$(this).parent().find(".mt-menu-sub-content").show();
+		}
+	});
 
 	/*tbody操作区显示*/
 	$("tbody").hover(function(){
@@ -6,5 +32,39 @@ $(function(){
 	},function(){
 		$(this).find(".lastOperates").hide();
 	});
+
+	/*全选选择框*/
+	$(".allSelector").click(function(){
+		var checkedState=$(this).attr("ischecked");
+		if (checkedState=="false") 
+		{
+			$(this).attr("ischecked","true");
+			$(".selector").attr("checked","true");
+		}
+		if (checkedState=="ture") 
+		{
+			$(this).attr("ischecked","false");
+			$(".selector").removeAttr("checked");
+		}
+	});
+
+	/*热卖单品*/
+	$(".probtnall").hover(function(){
+		var num=$(this).find("i").attr("num");
+		$(this).find("i").addClass("iconcolor");
+		$(".probo").hide();
+		$(".probo[num="+num+"]").show();
+	},function(){
+		$(this).find("i").removeClass("iconcolor");
+	});
+
+	/*猜你喜欢*/
+	$(".proitemmode").hover(function(){
+		$(this).css("border-color","#f40");
+		$(this).find(".prolitwo").show();
+	},function(){
+			$(this).css("border-color","");
+		$(this).find(".prolitwo").hide();
+	});
+
 });
-	
