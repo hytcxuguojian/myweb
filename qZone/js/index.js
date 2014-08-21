9064208bcde54aad5e2a9c9fa1aa41a4253d2384
zixuwuyou <1075737942@qq.com>
2014-08-21 23:38:30 +0800

    QQ空间第二天

--- qZone/js/index.js
+++ qZone/js/index.js
@@ -3,6 +3,15 @@ $(function(){
 	/******左侧菜单显示隐藏列表*****/
 	$(".tabSwitch").mouseover(function(){
 		$(this).hide();
-		$("#tab_hide_list").show();
+		$(this).parent().find(".tab_hide_list").show();
+	});
+
+	/***********发表说说文本框************/
+	$(".shuoshuo_editor").focus(function(){
+		$(".postfooter").slideDown(300);
+		return false;
+	});
+	$(".shuoshuo_editor").blur(function(){
+		$(".postfooter").slideUp(300);
 	});
 });
